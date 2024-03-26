3a15b28bccd81bc1bf270022980b48c93f426a84
eunkyoung99 <eunjee001@gmail.com>
2024-03-26 11:01:27 +0900

    기타ㅏ 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.kkyoungs.samplewatchapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.kkyoungs.samplewatchapp"
+        applicationId = "com.bple.hyundai.android"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         vectorDrawables {
